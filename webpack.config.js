2bcd328ae2c06b1d72485366d1d151ab80d6105e
lukikaaa <muslihakbar8@gmail.com>
2024-07-31 08:50:29 +0700

    Update webpack.config.js and fix image paths

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -25,6 +27,19 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/, // Loader untuk gambar
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+              publicPath: "images",
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
@@ -34,6 +49,9 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public/images", to: "images" }],
     }),
   ],
   optimization: {
